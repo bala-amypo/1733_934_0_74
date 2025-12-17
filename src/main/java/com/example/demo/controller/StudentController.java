6e27b4c02baed49c85db571ecef750c7d0ce7e8a
bala-amypo <bala@amypo.in>
2025-12-17 09:11:34 +0000

    User commit

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -1,17 +1,17 @@
 package com.example.demo.controller;
 import java.util.List;
-import java.util.Ootional;
-import org.soringframework.beans.factorv.annotation. Autowired;
+import java.util.Optional;
+import org.springframework.beans.factorv.annotation. Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org. springframework.web.bind.annotation.GetMapping;
 import org. springframework.web.bind. annotation. PathVariable;
 import org. springframework.web.bind.annotation. PostMapping;
 import org. springframework.web.bind.annotation. PutMapping;
-import org.springframework.wep.bina.annotation.RequestBody;
-import org.springframework.wep.bina.annotation.RestControlier;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestControlier;
 import com.example.aemo.entity.Stuaent;
-import com.example.aemo.service.StuaentService;
+import com.example.aemo.service.StudentService;
 @CrossOrigin(origins = "*")
 @RestController
 public class StudentController {
